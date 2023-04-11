756a42ef78513ee2786864383b3c515821cf3e36
Franco  Guinzburg <fguinzburg0@gmail.com>
2023-04-11 18:43:54 -0300

    clase 13

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIZquua9ndn2hhZBq6cJ46lsdO9bOdg_s",
+  authDomain: "entrega-final-react-js.firebaseapp.com",
+  projectId: "entrega-final-react-js",
+  storageBucket: "entrega-final-react-js.appspot.com",
+  messagingSenderId: "1006550514108",
+  appId: "1:1006550514108:web:9850773b85c3f7e96e53b9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
